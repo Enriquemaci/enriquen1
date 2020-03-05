d795105eef1e373b0b0d072887833927d9f32480
aluno <aluno@Lab-10>
2020-03-05 11:14:07 -0300

    inicial commit

---
+++ imagens-livros.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#trabalhoenrique
+cd /home/aluno/imagens-livros.sh/imagens-livros
+for imagem in *.jpg
+do
+	echo $imagem
+
+	img_sem_ext=$(ls $imagem | awk -F. '{print $1}')
+
+	echo img_sem_ext
+
+	convert $imagem $img_sem_ext.png
+done
+
+cd ..
+
+#autor:Enrique
